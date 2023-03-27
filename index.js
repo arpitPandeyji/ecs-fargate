5cde163587ea93ec6386f34a7dfa1e2f6f4a9ac0
arpitPandeyji <128712087+arpitPandeyji@users.noreply.github.com>
2023-03-27 19:49:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 app.set("port", process.env.PORT || 80);
 
 app.get('/', function(req, res){
-  res.send('Hello World to Cloud Tech !');
+  res.send('Hello World to Cloud Tech !!');
 });
 
 /* istanbul ignore next */
